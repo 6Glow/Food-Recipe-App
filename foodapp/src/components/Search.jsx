ef1b6989400ed001d9b9972164a21c57218bb2ad
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-10-10 12:54:50 +0300

    feat: implement useEffect hook for data fetching and side effects

---
+++ foodapp/src/components/Search.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function Search() {
+  const [query, setQuery] = useState("pizza");
+  useEffect(() => {
+    function demo() {
+      console.log("Demo function executed");
+    }
+    demo()
+  }, [query]);
+  return (
+    <div>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+}
