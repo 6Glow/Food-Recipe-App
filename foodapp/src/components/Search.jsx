a49c651f773d7c5620c263ee92fa2d1fb094fec9
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-10-10 15:09:02 +0300

    feat: add state management to save and handle food data using useState hook

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 const URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
   useEffect(() => {
     async function fetchFood(){
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`)
       const data = await res.json();
       console.log(data)
+      setFoodData(data.results)
     }
     fetchFood()
   }, [query]);
