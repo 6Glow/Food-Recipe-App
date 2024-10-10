688b6e19aef7e376d6158c2994134a55888d8f4a
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-10-10 19:09:39 +0300

    git commit -m feat: implement Search component to allow users to filter food items based on input

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import styles from "./search.module.css";
 const URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
@@ -15,8 +15,9 @@ export default function Search({ foodData, setFoodData }) {
     fetchFood()
   }, [query]);
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input
+      className={styles.input}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         type="text"
